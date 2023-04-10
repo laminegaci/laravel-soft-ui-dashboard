7deca754bc8a3d5826235a2cda0b9d92987b29b9
mohamed lamine gaci <mohamed61lamine@gmail.com>
2023-04-10 14:55:32 +0100

    install livewire

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Admin\Dashboard\Index;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', Index::class)->name('dashboard.index');
